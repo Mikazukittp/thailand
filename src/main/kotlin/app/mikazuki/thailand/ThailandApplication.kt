0b3a49a96a542d091d6d3ee452ab1fca4b8cb4b1
haijima <k.haijima@gmail.com>
2018-02-21 00:41:35 +0900

    Create project from Spring Initializr

---
+++ src/main/kotlin/app/mikazuki/thailand/ThailandApplication.kt
@@ -0,0 +1,11 @@
+package app.mikazuki.thailand
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class ThailandApplication
+
+fun main(args: Array<String>) {
+    runApplication<ThailandApplication>(*args)
+}
