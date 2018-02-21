281b488355cd949511f9e6de3602c521ad1873a4
haijima <k.haijima@gmail.com>
2018-02-22 07:59:49 +0900

    tmp

--- src/main/kotlin/app/mikazuki/thailand/ThailandApplication.kt
+++ src/main/kotlin/app/mikazuki/thailand/ThailandApplication.kt
@@ -1,18 +1,17 @@
 package app.mikazuki.thailand
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
-import org.springframework.boot.builder.SpringApplicationBuilder
 import org.springframework.boot.runApplication
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer
 
 @SpringBootApplication
 class ThailandApplication : SpringBootServletInitializer() {
 
-    override fun configure(builder: SpringApplicationBuilder?): SpringApplicationBuilder {
-        // Servletコンテナ起動時の設定クラス認識
-        // warファイル生成のため必要
-        return builder!!.sources(ThailandApplication::class.java)
-    }
+//    override fun configure(builder: SpringApplicationBuilder?): SpringApplicationBuilder {
+//        // Servletコンテナ起動時の設定クラス認識
+//        // warファイル生成のため必要
+//        return builder!!.sources(ThailandApplication::class.java)
+//    }
 }
 
 fun main(args: Array<String>) {
--- src/main/kotlin/app/mikazuki/thailand/misc/config/SecurityConfig.kt
+++ src/main/kotlin/app/mikazuki/thailand/misc/config/SecurityConfig.kt
@@ -2,29 +2,27 @@ package app.mikazuki.thailand.misc.config
 
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder
 import org.springframework.security.config.annotation.web.builders.HttpSecurity
-import org.springframework.security.config.annotation.web.builders.WebSecurity
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter
 import org.springframework.security.crypto.password.NoOpPasswordEncoder
 import org.springframework.security.crypto.password.PasswordEncoder
 
-
 @EnableWebSecurity
+@Configuration
 class SecurityConfig : WebSecurityConfigurerAdapter() {
-
-    override fun configure(web: WebSecurity?) {
-        super.configure(web)
-    }
 
     override fun configure(http: HttpSecurity?) {
         http?.let {
             it.authorizeRequests()
-                .antMatchers("/view/**").permitAll()
+                .antMatchers("login", "/view/**").permitAll()
                 .anyRequest().authenticated()
                 .and()
-                .formLogin()
+                .formLogin().defaultSuccessUrl("/")
+                .and()
+                .csrf().disable()
         }
     }
 
