d58fc9993fa94413a798263a2cc29af78b81a403
haijima <k.haijima@gmail.com>
2018-02-22 00:19:12 +0900

    Add GAE settings

--- src/main/kotlin/app/mikazuki/thailand/ThailandApplication.kt
+++ src/main/kotlin/app/mikazuki/thailand/ThailandApplication.kt
@@ -1,10 +1,19 @@
 package app.mikazuki.thailand
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.builder.SpringApplicationBuilder
 import org.springframework.boot.runApplication
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer
 
 @SpringBootApplication
-class ThailandApplication
+class ThailandApplication : SpringBootServletInitializer() {
+
+    override fun configure(builder: SpringApplicationBuilder?): SpringApplicationBuilder {
+        // Servletコンテナ起動時の設定クラス認識
+        // warファイル生成のため必要
+        return builder!!.sources(ThailandApplication::class.java)
+    }
+}
 
 fun main(args: Array<String>) {
     runApplication<ThailandApplication>(*args)
