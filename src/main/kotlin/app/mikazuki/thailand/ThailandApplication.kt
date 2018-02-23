37665ec71fcf181cadce6d0a02e9ff1e16c24fa9
haijima <k.haijima@gmail.com>
2018-02-24 01:08:43 +0900

    Bye GCP, Hello AWS

--- src/main/kotlin/app/mikazuki/thailand/ThailandApplication.kt
+++ src/main/kotlin/app/mikazuki/thailand/ThailandApplication.kt
@@ -2,10 +2,9 @@ package app.mikazuki.thailand
 
 import org.springframework.boot.SpringApplication
 import org.springframework.boot.autoconfigure.SpringBootApplication
-import org.springframework.boot.web.support.SpringBootServletInitializer
 
 @SpringBootApplication
-class ThailandApplication : SpringBootServletInitializer()
+class ThailandApplication
 
 fun main(args: Array<String>) {
     SpringApplication.run(ThailandApplication::class.java, *args)
