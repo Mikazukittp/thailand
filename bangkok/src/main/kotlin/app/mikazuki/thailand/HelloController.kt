310cdbff974d1fbb2bc5bdb3965ef6ec0e30baf7
TatsuyaIshibe <ishibe.tatsuya@gmail.com>
2018-03-04 00:23:40 +0900

    メール送信機能

--- bangkok/src/main/kotlin/app/mikazuki/thailand/HelloController.kt
+++ bangkok/src/main/kotlin/app/mikazuki/thailand/HelloController.kt
@@ -1,23 +1,18 @@
 package app.mikazuki.thailand
 
 import org.apache.commons.logging.LogFactory
-import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.stereotype.Controller
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.ResponseBody
 
 @Controller
-class HelloController  @Autowired constructor(private val mailSenderService: MailSenderService ){
+class HelloController {
 
     protected var logger = LogFactory.getLog(javaClass)
 
     @GetMapping("/")
     @ResponseBody
     fun hello(): String {
-
-        // TODO 直す
-        mailSenderService.send()
-
         return "Hello World!!"
     }
 }
