b8450395b52ac3e064fa13505615c467e02bd14a
TatsuyaIshibe <ishibe.tatsuya@gmail.com>
2018-03-04 00:23:40 +0900

    メール送信機能

--- bangkok/src/main/kotlin/app/mikazuki/thailand/MailSenderService.kt
+++ bangkok/src/main/kotlin/app/mikazuki/thailand/MailSenderService.kt
@@ -7,60 +7,49 @@ import com.amazonaws.services.simpleemail.model.*
 import org.springframework.stereotype.Service
 
 
+
 @Service
 class MailSenderService {
 
-    // TODO 変数は直す
-
-    // Replace sender@example.com with your "From" address.
-    // This address must be verified with Amazon SES.
-    val FROM = "mikazuki.ttp@gmail.com"
-
-    // Replace recipient@example.com with a "To" address. If your account
-    // is still in the sandbox, this address must be verified.
-    val TO = "mikazuki.ttp@gmail.com"
-
-    // The subject line for the email.
-    val SUBJECT = "Amazon SES test (AWS SDK for Java)"
-
-    // The HTML body for the email.
-    val HTMLBODY = ("<h1>Amazon SES test (AWS SDK for Java)</h1>"
-            + "<p>This email was sent with <a href='https://aws.amazon.com/ses/'>"
-            + "Amazon SES</a> using the <a href='https://aws.amazon.com/sdk-for-java/'>"
-            + "AWS SDK for Java</a>")
-
-    // The email body for recipients with non-HTML email clients.
-    val TEXTBODY = "This email was sent through Amazon SES " + "using the AWS SDK for Java."
+    val fromAddress = "mikazuki.ttp@gmail.com"
 
     private val ACCESS_KEY = "AKIAIZLAV3QSPGTJ7AGQ"
     private val SECRET_ACCESS_KEY = "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"
 
     fun send() {
+
+        // TODO 直す
+        val toAddress = "mikazuki.ttp@gmail.com"
+        val subject = "【テスト】出欠確認メール"
+        val htmlBody = ("<h1>Amazon SES test (AWS SDK for Java)</h1>"
+                + "<p>This email was sent with <a href='https://aws.amazon.com/ses/'>"
+                + "Amazon SES</a> using the <a href='https://aws.amazon.com/sdk-for-java/'>"
+                + "AWS SDK for Java</a>")
+        // The email body for recipients with non-HTML email clients.
+        val textBody = "This email was sent through Amazon SES " + "using the AWS SDK for Java."
+
+
         val client = AmazonSimpleEmailServiceClient(BasicAWSCredentials(ACCESS_KEY, SECRET_ACCESS_KEY))
                 .withRegion<AmazonSimpleEmailServiceClient>(Regions.US_WEST_2)
         val request = SendEmailRequest()
                 .withDestination(
-                        Destination().withToAddresses(TO))
+                        Destination().withToAddresses(toAddress))
                 .withMessage(Message()
                         .withBody(Body()
                                 .withHtml(Content()
-                                        .withCharset("UTF-8").withData(HTMLBODY))
+                                        .withCharset("UTF-8").withData(htmlBody))
                                 .withText(Content()
-                                        .withCharset("UTF-8").withData(TEXTBODY)))
+                                        .withCharset("UTF-8").withData(textBody)))
                         .withSubject(Content()
-                                .withCharset("UTF-8").withData(SUBJECT)))
-                .withSource(FROM)
+                                .withCharset("UTF-8").withData(subject)))
+                .withSource(fromAddress)
 
-        println("Hi!")
 
         try {
             client.sendEmail(request)
         } catch (e: Exception) {
             print(e)
         }
-
-        println("Email sent!")
-
 
     }
 }
