dc07adc1c0ec97c977a5d23ec30dd623afd1fffa
TatsuyaIshibe <ishibe.tatsuya@gmail.com>
2018-03-03 21:19:22 +0900

    AWS SDK導入とメール送信ロジック作成

--- bangkok/src/main/kotlin/app/mikazuki/thailand/HelloController.kt
+++ bangkok/src/main/kotlin/app/mikazuki/thailand/HelloController.kt
@@ -1,18 +1,23 @@
 package app.mikazuki.thailand
 
 import org.apache.commons.logging.LogFactory
+import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.stereotype.Controller
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.ResponseBody
 
 @Controller
-class HelloController {
+class HelloController  @Autowired constructor(private val mailSenderService: MailSenderService ){
 
     protected var logger = LogFactory.getLog(javaClass)
 
     @GetMapping("/")
     @ResponseBody
     fun hello(): String {
+
+        // TODO 直す
+        mailSenderService.send()
+
         return "Hello World!!"
     }
 }
