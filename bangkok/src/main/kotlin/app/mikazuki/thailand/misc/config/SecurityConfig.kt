f0707096284bb654040d7182fe39dedd4796b339
haijima <k.haijima@gmail.com>
2018-03-04 15:08:44 +0900

    WIP2 setting to deploy

--- bangkok/src/main/kotlin/app/mikazuki/thailand/misc/config/SecurityConfig.kt
+++ bangkok/src/main/kotlin/app/mikazuki/thailand/misc/config/SecurityConfig.kt
@@ -2,24 +2,27 @@ package app.mikazuki.thailand.misc.config
 
 import app.mikazuki.thailand.user.UserService
 import org.springframework.beans.factory.annotation.Autowired
-import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder
 import org.springframework.security.config.annotation.web.builders.HttpSecurity
+import org.springframework.security.config.annotation.web.builders.WebSecurity
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter
-import org.springframework.security.crypto.password.NoOpPasswordEncoder
-import org.springframework.security.crypto.password.PasswordEncoder
-
 
 @EnableWebSecurity
 @Configuration
 class SecurityConfig @Autowired constructor(private val userDetailsService: UserService) : WebSecurityConfigurerAdapter() {
 
+    override fun configure(web: WebSecurity?) {
+        super.configure(web)
+        web?.ignoring()?.antMatchers("/css/**", "/js/**", "/img/**")
+    }
+
     override fun configure(http: HttpSecurity?) {
         http?.let {
             it.authorizeRequests()
                 .antMatchers("/login").permitAll()
+                .antMatchers("/parties/**").permitAll()
                 .anyRequest().authenticated()
                 .and()
                 .formLogin().defaultSuccessUrl("/user")
@@ -30,11 +33,10 @@ class SecurityConfig @Autowired constructor(private val userDetailsService: User
         auth?.userDetailsService(userDetailsService)
     }
 
-    @Bean
-    fun passwordEncoder(): PasswordEncoder {
-        // TODO: テスト用にハッシュ化していない
-        return NoOpPasswordEncoder.getInstance()
-    }
-
+//    @Bean
+//    fun passwordEncoder(): PasswordEncoder {
+//        // TODO: テスト用にハッシュ化していない
+//        return NoOpPasswordEncoder.getInstance()
+//    }
 
 }
